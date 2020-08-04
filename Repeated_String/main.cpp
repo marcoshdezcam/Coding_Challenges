31e52c911b7adb15853bef0ae49074c1a1094b52
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-03 22:04:30 -0500

    Setup GitFlow

---
+++ Repeated_String/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+long repeatedString(string s, long n) {
+    // s.size() mod n = finalPos;
+    // As_untilFinalPos = s.find(')
+    // n / s.size() = numberOfRepetitions
+    // numberOf_A = s.find('a')
+    // total_As = (numberOf_A * numberOfRepetitions) + (As_untilP)
+}
+
+int main(){
+    return 0;
+}
