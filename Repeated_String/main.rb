63edd0788f9948b3f8f734670b20bcb1df66dd9d
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-18 11:08:18 -0500

    Repeated string challenge solved!

--- Repeated_String/main.rb
+++ Repeated_String/main.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-def repeated_string(s, n)
-  # counter = 0
-  counter = n / s.size
-  remainder = n % s.size
-  p remainder
-  i = 0
-  latest_as = s[i] == 'a' ? latest_as + s[i] : nil && i += while i <= remainder
-  p latest_as
-  number_of_as = s.scan(/a/)
-  counter *= number_of_as.size
+def repeated_string(infiniteString, firstLetters)
+  remainder = firstLetters % infiniteString.size
+  counter = ( firstLetters / infiniteString.size ) * infiniteString.scan(/a/).size + infiniteString[0...remainder].scan(/a/).size
   counter
 end
-# [ abaabaaba a ]
 
-puts repeated_string('aba', 10)
+repeated_string('aba', 12)
