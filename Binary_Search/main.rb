a9d41cacd1ca1bdf7c6c9e02bf36ec9e5362a466
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-19 09:47:20 -0500

    Binary search solved!

--- Binary_Search/main.rb
+++ Binary_Search/main.rb
@@ -4,8 +4,16 @@ def sqrt(number)
 end
 
 def sqrt_recursive(number, min_interval, max_interval)
-  # Your code here
+  middle = ( max_interval - min_interval ) / 2 + min_interval
+  sqrt = middle * middle
 
+  if sqrt == number
+    middle
+  elsif sqrt < number
+    sqrt_recursive(number, middle, max_interval)
+  elsif sqrt > number
+    sqrt_recursive(number, min_interval, middle)
+  end
 end
 
 puts sqrt(25)
