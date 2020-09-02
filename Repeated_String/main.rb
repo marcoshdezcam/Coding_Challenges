bd118af731d4da26816329a44295a66421b41e84
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-02 02:43:59 -0500

    Coding challenge solved on all test cases

--- Repeated_String/main.rb
+++ Repeated_String/main.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-def repeated_string(infinite_string, first_letters)
-  remainder = first_letters % infinite_string.size
-  counter = (first_letters / infinite_string.size) * infinite_string.scan(/a/).size + infinite_string[0...remainder].scan(/a/).size
+def repeated_string(string, first_letters)
+  remainder = first_letters % string.size
+  counter = (first_letters / string.size) * string.scan(/a/).size + string[0...remainder].scan(/a/).size
   counter
 end
 
