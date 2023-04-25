40bb84ee0af4260e0f07bfb0f474bd605c738c67
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2023-04-24 18:38:09 -0600

    FIX: IF Else java file.

---
+++ Java/src/IfElse.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class IfElse {
+    public IfElse() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println(N);
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else if (N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if (N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else if (N > 20) {
+            System.out.println("Not Weird");
+        }
+
+    }
+}
