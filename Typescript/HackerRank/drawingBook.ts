736795303733d347f435814126e128fa89ff52ce
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2024-04-23 13:01:07 -0600

    Missing edge cases.
    7, 3 = 1
    5, 4 = 1

--- Typescript/HackerRank/drawingBook.ts
+++ Typescript/HackerRank/drawingBook.ts
@@ -6,16 +6,29 @@ enum StartPos {
 function pageCount(n: number, p: number): number {
   // Write your code here
   let totalPages: number = n,
-    pageObj = p,
+    pageObjective = p,
     minPagesToTurn = 0;
   let startFrom: StartPos =
-    pageObj < Math.floor(totalPages / 2) ? StartPos.Beginning : StartPos.End;
+    pageObjective < Math.floor(totalPages / 2)
+      ? StartPos.Beginning
+      : StartPos.End;
 
   if (startFrom === StartPos.Beginning) {
-    minPagesToTurn = Math.floor(pageObj / 2);
+    minPagesToTurn = Math.floor(pageObjective / 2);
+    console.log({
+      "Starging from": startFrom,
+      minPagesToTurn,
+    });
   } else if (startFrom === StartPos.End) {
-    minPagesToTurn = Math.floor((totalPages - pageObj) / 2);
+    minPagesToTurn = Math.floor((totalPages - pageObjective) / 2);
+    console.log({
+      "Starging from": startFrom,
+      minPagesToTurn,
+    });
   }
 
   return minPagesToTurn;
 }
+
+console.log(pageCount(7, 3));
+console.log(pageCount(5, 4));
