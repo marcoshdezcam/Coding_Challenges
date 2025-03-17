4bef9ac768dd0013fc2917b8c618b6be0057e4db
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2025-03-16 20:13:27 -0600

    Versatile Sum passing all tests.

---
+++ Typescript/ProblemSolving/VersatileSum/versatileSum.test.ts
@@ -0,0 +1,34 @@
+import versatileSum from "./versatileSum";
+
+describe("Base cases:", () => {
+  it("Should return 7: ", () => {
+    expect(versatileSum(3, 4)).toBe(7);
+  });
+
+  it("Should return: ", () => {
+    expect(versatileSum(10, 12)).toEqual(22)
+  })
+
+  it("Should return: ", () => {
+    expect(versatileSum(3)(7)).toEqual(10)
+  })
+
+  it("Should return: ", () => {
+    expect(versatileSum(3)()(4)).toEqual(7)
+  })
+
+  it("Should return: ", () => {
+    expect(versatileSum(5)()()()()(10)).toEqual(15)
+  })
+
+  it("Should return: ", () => {
+    expect(versatileSum()(3)()(4)).toEqual(7)
+  })
+
+  it("Should return: ", () => {
+    expect(versatileSum()()()(5)()()()()()(10)).toEqual(15)
+  })
+});
+
+
+
--- Typescript/ProblemSolving/VersatileSum/versatileSum.ts
+++ Typescript/ProblemSolving/VersatileSum/versatileSum.ts
@@ -1,19 +1,14 @@
-export function versatileSum(num1: number, num2?: number): any {
+export default function versatileSum(num1?: number, num2?: number): any {
+  if (num1 === undefined) return versatileSum;
+
   if (num2 === undefined) {
-    return function (num3: number) {
+    return function innerAddition(num3?: number): any {
+      if (num3 === undefined) {
+        return innerAddition;
+      }
       return num1 + num3;
     }
   }
 
   return num1 + num2;
 }
-
-const total1: number = versatileSum(3, 4);
-const total2: number = versatileSum(10, 12);
-const total3: number = versatileSum(3)(7);
-
-console.log({
-  total1,
-  total2,
-  total3
-})
