bfb4a939b9097e265d5540f89f441b2a710b32b0
Marcos Hernández <work@marcoshc.dev>
2026-03-03 06:58:34 -0600

    TS: Check if is Unicode palindrome

--- Typescript/Cracking & Grokking/validPalindrome.ts
+++ Typescript/Cracking & Grokking/validPalindrome.ts
@@ -1,10 +1,27 @@
-function isPalidrome(s: string): true | false {
-  // Using Two-Pointer Technique
+function isAlphanumeric(char: string): boolean {
+  return /[\p{L}\p{N}]/u.test(char);
+}
+
+function isAlphabeticPalindrome(s: string): boolean {
   let left = 0,
     right = s.length - 1;
+  console.log({
+    s,
+  });
 
   while (left < right) {
-    if (s[left] !== s[right]) return false;
+    // Skip non-alphanumeric characters from the left
+    while (left < right && !isAlphanumeric(s[left])) {
+      left++;
+    }
+    // Skip non-alphanumeric characters from the right
+    while (left < right && !isAlphanumeric(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
 
     left++;
     right--;
@@ -12,3 +29,8 @@ function isPalidrome(s: string): true | false {
 
   return true;
 }
+
+console.log({
+  testcase1: isAlphabeticPalindrome('Z'),
+  testCase2: isAlphabeticPalindrome(`abc123cba`),
+});
